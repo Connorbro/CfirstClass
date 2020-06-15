dd5f646d8ab27d449e17109eb6ada05295fdbf1d
connor <vbrookesy@hotmail.co.uk>
2020-06-15 12:26:05 +0100

    Added divide multiply...

--- Program.cs
+++ Program.cs
@@ -8,9 +8,23 @@ namespace introduction
 {
     class Mathematics
     {
+        // Encapsulation
         public int Addition(int numberOne, int numberTwo) //Capitalised Methods are best!
         {
             return numberOne + numberTwo;
+        }
+
+        public int Subtraction(int numberOne, int numberTwo)
+        {
+            return numberOne - numberTwo;
+        }
+        public double Division(double numberOne, double numberTwo)
+        {
+            return numberOne / numberTwo;
+        }
+        public int Multiplication(int numberOne, int numberTwo)
+        {
+            return numberOne * numberTwo;
         }
     }
 
@@ -21,8 +35,14 @@ namespace introduction
     {
         static void Main(string[] args)
         {
+            Mathematics maths = new Mathematics();
+            int subtractPlease = maths.Subtraction(200, 32);
+            int addMePlease = maths.Addition(10, 40);
+            int multiplyMePlease = maths.Multiplication(22, 3);
+            double divideMePlease = maths.Division(33, 2.2);
             
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(subtractPlease);
+            Console.WriteLine(divideMePlease);
         }
     }
 }
