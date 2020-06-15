f466cef7d27e81a4c565e740555c2af812b40a7b
connor <vbrookesy@hotmail.co.uk>
2020-06-15 11:15:19 +0100

    Addition method

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+// a library that will be essential for the majority of the first week...
+// without it... we will have lengthy paths
+
+namespace introduction
+// Any class inside of the namespace will be available...
+
+{
+    class Mathematics
+    {
+        public int Addition(int numberOne, int numberTwo) //Capitalised Methods are best!
+        {
+            return numberOne + numberTwo;
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
